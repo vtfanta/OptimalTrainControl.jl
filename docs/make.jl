20ea6731a46098eafc572103f0624e0bfe8d80e8
vtfanta <fantavit@fel.cvut.cz>
2024-05-10 11:02:23 +0200

    Fix docs deploy URL

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ DocMeta.setdocmeta!(OptimalTrainControl, :DocTestSetup, :(using OptimalTrainCont
 makedocs(;
     modules=[OptimalTrainControl],
     authors="Vít Fanta <fantavit@fel.cvut.cz> and contributors",
-    repo="https://github.com/vtfanta/OptimalTrainControl_v2.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/vtfanta/OptimalTrainControl.jl/blob/{commit}{path}#{line}",
     sitename="OptimalTrainControl.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
@@ -22,5 +22,5 @@ makedocs(;
 )
 
 deploydocs(
-    repo = "github.com/vtfanta/OptimalTrainControl_v2.jl.git",
+    repo = "github.com/vtfanta/OptimalTrainControl.jl.git",
 )
